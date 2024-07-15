8f034e4e992f8aca05ae017846c03334b5765203
alex2000d <demarcoalessandro777@gmail.com>
2024-07-15 14:42:25 +0200

    [UPD] color changed on the button like on click event

--- main.js
+++ main.js
@@ -1,59 +1,59 @@
 // creo il mio arrey di oggetti
 const posts = [
     {
-        id: 1,
-        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        media: "https://unsplash.it/600/300?image=171",
-        author: {
-            name: "Phil Mangione",
-            image: "https://unsplash.it/300/300?image=15"
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+       "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
         },
-        likes: 80,
-        created: "2021-06-25"
+        "likes": 80,
+        "created": "2021-06-25"
     },
     {
-        id: 2,
-        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        media: "https://unsplash.it/600/400?image=112",
-        author: {
-            name: "Sofia Perlari",
-            image: "https://unsplash.it/300/300?image=10"
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
         },
-        likes: 120,
-        created: "2021-09-03"
+        "likes": 120,
+        "created": "2021-09-03"
     },
     {
-        id: 3,
-        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        media: "https://unsplash.it/600/400?image=234",
-        author: {
-            name: "Chiara Passaro",
-            image: "https://unsplash.it/300/300?image=20"
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
         },
-        likes: 78,
-        created: "2021-05-15"
+        "likes": 78,
+        "created": "2021-05-15"
     },
     {
-        id: 4,
-        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        media: "https://unsplash.it/600/400?image=24",
-        author: {
-            name: "Luca Formicola",
-            image: null
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
         },
-        likes: 56,
-        created: "2021-04-03"
+        "likes": 56,
+        "created": "2021-04-03"
     },
     {
-        id: 5,
-        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        media: "https://unsplash.it/600/400?image=534",
-        author: {
-            name: "Alessandro Sainato",
-            image: "https://unsplash.it/300/300?image=29"
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
         },
-        likes: 95,
-        created: "2021-03-05"
+        "likes": 95,
+        "created": "2021-03-05"
     }
 ];
 
@@ -68,7 +68,7 @@ posts.forEach((element) =>{
     <div class="post__header">
         <div class="post-meta">                    
             <div class="post-meta__icon">
-                <img class="profile-pic" src="${(author.image)}" alt="${(author.image)}">                    
+                <img class="profile-pic" src="${(author.image)}" alt="">                    
             </div>
             <div class="post-meta__data">
                 <div class="post-meta__author">${(author.name)}</div>
@@ -78,12 +78,12 @@ posts.forEach((element) =>{
     </div>
     <div class="post__text">${content}</div>
     <div class="post__image">
-        <img src="${media}" alt="${media}">
+        <img src="${media}" alt="">
     </div>
     <div class="post__footer">
         <div class="likes js-likes">
             <div class="likes__cta">
-                <a id="like" class="like-button  js-like-button" href="#" data-postid="${id}">
+                <a class="like-button  js-like-button" href="#" data-postid="${id}">
                     <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                     <span class="like-button__label">Mi Piace</span>
                 </a>
@@ -99,4 +99,21 @@ posts.forEach((element) =>{
 
 // creo il mio arrey per i like
 const like = [];
+
+// recupero il mio pulsante dal dom
+const likes = document.querySelectorAll('.js-like-button');
+// recupero il mio counter like
+const likes__counter = document.querySelectorAll('.js-likes-counter')
+//  creo un ciclo per i pulsanti mi piace
+for(let i = 0; i < likes.length; i++){
+    let button = likes[i];
+// faccio un evento click per il mio pulsante mi piace
+    button.addEventListener('click', function (event) {
+        // evito che al click del pulsante mi riporta all'inizio della pagina
+        event.preventDefault();
+        // aggiungo la classe al mio pulsante
+       this.classList.add('like-button--liked')
+})
+}
+
 
